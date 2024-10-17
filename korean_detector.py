666c848f7f8dd89a7126066eff2733338c674a60
Feralthedogg <153323053+Feralthedogg@users.noreply.github.com>
2024-10-18 02:07:16 +0900

    Update korean_detector.py

--- korean_detector.py
+++ korean_detector.py
@@ -1,6 +1,7 @@
 import re
 import asyncio
-
+from korean_keywords import korean_keywords
+from english_keywords import english_keywords
 
 class KoreanDetector:
     korean_pattern = re.compile(r'[가-힣]')
@@ -11,7 +12,7 @@ class KoreanDetector:
         '이', '가', '을', '를', '에', '에서', '으로', '로', '와', '과', '하고', '의',
         '도', '만', '은', '는', '까지', '조차', '밖에', '다', '입니다', '요', '죠', '지요',
         '네요', '십시오', '고', '서', '며', '면서', '지만', '는데', 'ㄴ', '는', 'ㄹ', '을', '던',
-        '그리고', '그러나', '그래서', '그러므로'
+        '그리고', '그러나', '그래서', '그러므로', '했어요', '어'
     }
 
     english_particles = {
@@ -39,7 +40,7 @@ class KoreanDetector:
         if text.isdigit():
             return self.korean_message
 
-        text_cleaned = re.sub(r'[\d\s\W_]+', '', text)
+        text_cleaned = re.sub(r'[\s]+', '', text)
 
         korean_count = len(re.findall(self.korean_pattern, text_cleaned))
         english_count = len(re.findall(self.english_pattern, text_cleaned))
@@ -51,26 +52,37 @@ class KoreanDetector:
         korean_ratio = korean_count / total_count
         english_ratio = english_count / total_count
 
-        contains_unknown = bool(re.findall(self.unknown_pattern, text_cleaned))
-
-        contains_korean_particles = any(particle in text_cleaned for particle in self.korean_particles)
-        contains_english_particles = any(particle in text_cleaned for particle in self.english_particles)
-
-        if contains_unknown:
-            if contains_korean_particles and korean_ratio >= english_ratio:
-                return self.korean_message
-            elif contains_english_particles and english_ratio > korean_ratio:
-                return self.english_message
-            else:
-                return self.unknown_message
-        elif english_ratio > 0.6 and not contains_korean_particles:
+        if english_ratio >= 0.8:
             return self.english_message
-        elif english_ratio > 0.6 and contains_korean_particles and english_count > korean_count * 2:
-            return self.english_message
-        elif korean_ratio > 0.5 or contains_korean_particles:
+        elif korean_ratio >= 0.8:
             return self.korean_message
-        else:
-            return self.english_message if english_ratio >= korean_ratio else self.korean_message
+
+        korean_weight = self._calculate_weight(text_cleaned, self.korean_particles, korean_keywords, ratio=korean_ratio)
+        english_weight = self._calculate_weight(text_cleaned, self.english_particles, english_keywords, ratio=english_ratio)
+
+        korean_weight += self._check_end_of_sentence(text_cleaned, self.korean_pattern)
+        english_weight += self._check_end_of_sentence(text_cleaned, self.english_pattern)
+
+        if korean_weight > english_weight:
+            return self.korean_message
+        elif english_weight > korean_weight:
+            return self.english_message
+
+        return self.unknown_message
+
+    def _calculate_weight(self, text, particles, keywords, ratio):
+        weight = ratio
+        for particle in particles:
+            if particle in text:
+                weight += 1.5
+        for keyword in keywords:
+            if keyword in text:
+                weight += 0.5
+        return weight
+
+    def _check_end_of_sentence(self, text, pattern):
+        end_check = re.search(pattern, text[-1:])
+        return 2.0 if end_check else 0.0
 
     async def _detect_multiple_languages(self, texts):
         tasks = [self._detect_single_language(text) for text in texts]
